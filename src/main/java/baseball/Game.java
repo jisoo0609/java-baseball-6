0c7f9140675189da72c50ee88fe751229c1dfa08
JISOO <jisu20000609@gmail.com>
2025-10-15 18:28:27 +0900

    feat: 난수 3개 생성, 중복 제거

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,10 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 게임 상태 관리, 게임 루프 핵심
 public class Game {
+    private List<Integer> computerNumbers;
+
     // 게임 시작, 루프 실행, 재시작 종료 처리
     public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         // 난수 3개 생성, 중복 제거
+        computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9); // 난수 생성
+            // 만약 computerNumbers에 생성된 randomNumber가 없으면 추가
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+
+        }
 
         // 한 턴 동안 사용자 입력 → 판정 → 결과 출력
 
