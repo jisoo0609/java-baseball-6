07dbaaa2cf8db01f4a74098fc14e0d75ea2215f4
JISOO <jisu20000609@gmail.com>
2025-10-15 18:37:27 +0900

    feat: InputValidator.java validate

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -2,4 +2,12 @@ package baseball;
 
 // 사용자 입력 검증, 예외 처리
 public class InputValidator {
+    public static void validate(String input) {
+        checkLength(input);
+        checkDigits(input);
+        checkRange(input);
+        checkDuplicate(input);
+    }
+
+
 }
